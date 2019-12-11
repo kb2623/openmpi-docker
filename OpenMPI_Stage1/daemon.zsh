c43ac8825796ddf56748fe82ca2ece64e7ebd51d
Klemen@Haxor <roxor1992@gmail.com>
2019-12-11 01:47:26 +0100

    Added new fixes

--- OpenMPI_Stage1/daemon.zsh
+++ OpenMPI_Stage1/daemon.zsh
@@ -1,15 +1,26 @@
 #!/bin/zsh
 
+readonly BEXPORTFS='/usr/sbin/exportfs'
+readonly BIDMAPD='/usr/sbin/rpc.idmapd'
+readonly BMOUNTD='/usr/sbin/rpc.mountd'
+readonly BNFSD='/usr/sbin/rpc.nfsd'
+readonly BRPCBIND='/sbin/rpcbind'
+readonly BRPCINFO='/sbin/rpcinfo'
+readonly BRPC_SVCGSSD='/usr/sbin/rpc.svcgssd'
+readonly BSTATD='/sbin/rpc.statd'
+
 /usr/sbin/sshd -D -e -f /etc/ssh/sshd_config &
 if [ ! ${NODE_ID} = "0" ]; then exit 0; fi
 
-/sbin/rpcbind -w
-/sbin/rpcinfo
-/usr/sbin/rpc.nfsd --debug 8 --no-udp --no-nfs-version 2 --no-nfs-version 3
-if /usr/sbin/exportfs -rv; then
-	/usr/sbin/exportfs
+# TODO https://github.com/ehough/docker-nfs-server/blob/develop/entrypoint.sh
+
+$BRPCBIND -w
+$BRPCINFO
+if $BEXPORTFS -rv; then
+	$BEXPORTFS
 else
 	echo "Export validation failed, exiting..."
 	exit 1
 fi
-/usr/sbin/rpc.mountd --debug all --no-udp --no-nfs-version 2 --no-nfs-version 3
+$BMOUNTD --debug all --no-udp --no-nfs-version 2 --no-nfs-version 3
+$BNFSD --debug 8 --no-udp --no-nfs-version 2 --no-nfs-version 3
