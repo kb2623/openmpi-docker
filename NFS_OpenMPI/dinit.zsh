8dcbb7b234509a5ef2ca48608547873501563bdd
Klemen@Haxor <roxor1992@gmail.com>
2019-12-16 22:05:54 +0100

    Adde new fixes for NFSv4

--- NFS_OpenMPI/dinit.zsh
+++ NFS_OpenMPI/dinit.zsh
@@ -2,77 +2,82 @@
 
 PARAMS_FILE=$1
 
-# Arguments -----------------------------------------------------------------------------------------------------
+function fHCutLine {
+	if [ $# -lt 2 ]; then exit 1; fi
+	echo $1 | tr '\t' ' ' | tr -s ' ' | cut -d' ' -f$2
+}
+
+function fHCutFile {
+	if [ $# -lt 3 ]; then exit 1; fi
+	line=$(cat $1 | head -$(($2+1)) | tail -1)
+	fHCutLine $line $3
+}
+
 function param {
 	cut -d' ' -f$1 $PARAMS_FILE
 }
 
+# Arguments -----------------------------------------------------------------------------------------------------
 HOST_FILE=$(param 1)
 NODE_ID=$(param 2)
 AUSER=$(param 3)
 AGROUP=$(param 4)
 WORD_DIR=$(param 5)
 
-echo $HOST_FILE $NODE_ID $AUSER $AGROUP $WORD_DIR
-
 # Constants -----------------------------------------------------------------------------------------------------
 readonly SSH_CONFIG='/etc/ssh/sshd_config'
 
-readonly BSSHD='/usr/sbin/sshd'
-readonly BEXPORTFS='/usr/sbin/exportfs'
-readonly BIDMAPD='/usr/sbin/rpc.idmapd'
-readonly BMOUNTD='/usr/sbin/rpc.mountd'
-readonly BNFSD='/usr/sbin/rpc.nfsd'
-readonly BRPCBIND='/sbin/rpcbind'
-readonly BRPCINFO='/sbin/rpcinfo'
-readonly BRPC_SVCGSSD='/usr/sbin/rpc.svcgssd'
-readonly BSTATD='/sbin/rpc.statd'
+readonly SSHD='/usr/sbin/sshd'
+readonly EXPORTFS='/usr/sbin/exportfs'
+readonly RPC_MOUNTD='/usr/sbin/rpc.mountd'
+readonly RPC_IDMAPD='/usr/sbin/rpc.idmapd'
+readonly RPC_SVCGSSD='/usr/sbin/rpc.svcgssd'
+readonly RPC_NFSD='/usr/sbin/rpc.nfsd'
+readonly RPC_GSSD='/usr/sbin/rpc.gssd'
+readonly RPCBIND='/sbin/rpcbind'
+readonly RPCINFO='/sbin/rpcinfo'
+readonly STATD='/sbin/rpc.statd'
 
 # Mount NFS ------------------------------------------------------------------------------------------------------
 function run_mound_nfs {
 	mkdir -p $WORD_DIR
 	chmod a+wrx $WORD_DIR
 	chown -R $AUSER:$AGROUP $WORD_DIR
-	mount -v -o nolock $(cat $HOST_FILE | tr '\t' ' ' | tr -s ' ' | cut -d' ' -f1 | head -1 | tail -1):/ $WORD_DIR
+	mount -v -o nolock $(fHCutFile $HOST_FILE 0 2):/ $WORD_DIR
 }
 
 # Services -------------------------------------------------------------------------------------------------------
 function run_sshd {
-	nohup $BSSHD -D -e -f $SSH_CONFIG &
+	nohup $SSHD -D -e -f $SSH_CONFIG &
 }
 
 function run_nfsd_server {
-	$BRPCBIND -w
-	$BRPCINFO
-	if $BEXPORTFS -rv; then
-		$BEXPORTFS
+	$RPCBIND -w
+	$RPCINFO
+	if $EXPORTFS -rv; then
+		$EXPORTFS
 	else
 		echo "Export validation failed, exiting..."
 		exit 1
 	fi
-	nohup $BMOUNTD -V 4 &
-	nohup $BIDMAPD &
-	nohup $BRPC_SVCGSSD &
-	nohup $BNFSD -u -t -V 4 -p 2049 8 &
-}
-
-function run_nfsd_client {
-	# FIXME najdi programe ter preveri ce delujejo
-	rpc.gssd -m
-	rpc.idmapd
+	$RPC_MOUNTD -V 4 
+	$RPC_IDMAPD
+	$RPC_SVCGSSD
+	$RPC_NFSD -u -t -V 4 -p 2049 8
 }
 
 # Type of Nodes ---------------------------------------------------------------------------------------------------
 function run_master {
 	run_sshd
-	# run_nfsd_server
-	# run_nfsd_client
+	run_nfsd_server
+	$RPC_GSSD -M -D
 	run_mound_nfs
 }
 
 function run_node {
 	run_sshd
-	# run_nfsd_client
+	$RPC_IDMAPD
+	$RPC_GSSD -M -D
 	run_mound_nfs
 }
 
